0370f6bfc9dbcd712cba8ddf83c2277e3bc15183
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-04-17 06:08:18 +0300

    initial commit

---
+++ wp-content/themes/shopall/page/subscription-gifting.php
@@ -0,0 +1,83 @@
+<section class="only-foto-subscription-gifting"></section>
+<section>
+	<div></div>
+</section>
+<section class="subscription-gifting-kosss">
+	<h3><b>SEND THEM EXTRAORDINARY FLORALS WITH A MONTHLY FLOWER SUBSCRIPTION</b></h3>
+	<h4><b>FLOWERS FOR EVERY OCCASION</b></h4>
+	<p>From birthdays to new beginnings, sending a flower subscription is the perfect way to help them celebrate. Gift high-quality, seasonal florals on repeat for just £40 a month, with a signature vase alongside their first delivery.</p>
+  	<div class="last-section-contant-block-kosss">
+		<div class="last-section-contant-block-kosss-card">
+			<div class="only-foto-subscription-gifting-img only-foto-subscription-gifting-one"></div>
+  			<h3>GIFT A FLOWER SUBSCRIPTION GIFT CARD</h3>
+			<p>For a polished present that keeps on giving and feels like pure luxury, nothing beats sending them a subscription gift card. Presented in our signature gift box and finished with a ribbon, it's the perfect go-to for flower lovers everywhere.</p>
+  			<a href="#">GIFT INSTANTLY</a>
+		</div>
+		<div class="last-section-contant-block-kosss-card ">
+			<div class="only-foto-subscription-gifting-img only-foto-subscription-gifting-second"></div>
+  			<h3>SUBSCRIBE YOURSELF TO MONTHLY FLOWERS</h3>
+			<p>Ideal for those that love a little floral boost, a flower subscription isn't just for gifting. Embrace new varieties and find moments of calm to arrange and admire, as your regular FLOWERRBX delivery becomes the highlight of your month.</p>
+  			<a href="/flower-subscriptions/">START YOUR SUBSCRIPTION</a>
+		</div>
+		<div class="last-section-contant-block-kosss-card ">
+			<div class="only-foto-subscription-gifting-img only-foto-subscription-gifting-threth"></div>
+  			<h3>SEND THEM A DIGITAL SUBSCRIPTION</h3>
+			<p>Whether it's a last-minute birthday gift or a surprise for a friend from afar, a digital flower subscription suits those unexpected present-giving moments. Shop on-site, and we'll send their subscription straight to their inbox, for an instant floral gift they'll love. </p>
+  			<a href="#">GIFT INSTANTLY</a>
+		</div>
+
+	</div>
+</section>
+
+<section class="subst-gifting-block-cartgift">
+	<div class="subst-gifting-block-cartgift-foto">
+		<div class="subst-gifting-block-cartgift-foto-1"></div>
+		<div class="subst-gifting-block-cartgift-foto-2"></div>
+		<div class="subst-gifting-block-cartgift-foto-3"></div>
+	</div>
+	<a href="/flower-subscriptions/">start your subscription</a>
+</section>
+<section class="block-reviews-and-photos sections-subscriptions-five">
+	<div class="block-reviews-and-photos-contant">
+		<h2>what our community <br> is saying about us</h2>
+		<div class="block-reviews-and-photos-contant-rev">
+			<div>
+				<div class="foto-stars-rewius"></div>
+  				<span>
+					<p> i ordered for my mum. beutiful colour, easy ordering, easy delivery. always chic</p>
+				</span>
+				<b><i><h4>EMELY</h4></i></b>
+			</div>
+			<div>
+				<div class="foto-stars-rewius"></div>
+  				<span>
+					<p>The colour and quality is amazing! my girl was so happy. cant recommend enough!</p>
+				</span>
+				<b><i><h4>SAM</h4></i></b>
+			</div>
+
+		</div>
+	</div>
+	<div class="sections-subscriptions-six-img"></div>
+</section>
+
+<section class="sections-foto-and-many-contant subsgift-antipapping">
+	<div class="sections-foto-subsgift-foto "></div>
+  	<div class="sections-foto-and-many-contant-text">
+		
+		<p><b>How does your subscription gift card work?</b></p>
+		<p>A FLOWERBX Subscription Gift Card works by helping you gift flowers to a loved one via digital or physical formats. Simply select your duration from 3, 6, or 12 months of our Classic flower subscription, and we'll deliver a physical gift card to your - or their - home, or a virtual voucher direct to their inbox.</p>
+  		<p><b>Can my recipient select their start date?</b></p>
+		<p>Yes! Your recipient can choose their start date. Once you have gifted them a Subscription Gift Card, they can use it to start a Classic subscription at a time to suit their schedule.</p>
+		<p><b>What flowers will I receive with my subscription?</b></p>
+		<p>Your subscription will include signature FLOWERBX flowers. These will include customer favourite single stem bouquets such as roses, tulips, peonies, hydrangeas, and more - dependant on seasonality. </p>
+		<p><b>How does payment work?</b></p>
+		<p>When you purchase a Subscription Gift Card, you will make payment for the duration you would like to gift. This may be 3, 6 or 12 months. Should your recipient like to extend their subscription, they can renew it at the end of their gifted duration.</p>
+		<p><b>Can I select my flowers?</b></p>
+		<p>Yes, you can select your flowers. Your online FLOWERBX flower account allows you to take your pick from the seasonal choices available.</p>
+		<p><b>Can I change the address or date on my subscription?</b></p>
+		<p>If you need to make any changes to your subscription, whether its the date, address or frequency please contact our customer service team who will be happy to help make any changes for you.</p>
+		<p><b>Can I buy a flower subscription as a gift for someone else?</b></p>
+		<p>Yes! You can buy a subscription for yourself or for someone else. Our Subscription Gift Card allows you to send flowers to a loved one on repeat, with a complimentary vase alongside their first delivery.</p>
+	</div>
+</section>
