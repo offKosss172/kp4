ebb511dabce7a4f6b9d42c468a29293a41c0b191
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-04-18 01:06:25 +0300

    initial commit

--- wp-content/themes/shopall/footer.php
+++ wp-content/themes/shopall/footer.php
@@ -11,14 +11,54 @@
                 <a href="/international-flower-delivery/">INTERNATIONAL FLOWER DELIVERY</a>
             </div>
             <div class="footer-kosss-head-subscrip">
-                <h3>GET THE FLOWERBX NEWSLETTER</h3> 
-                <form action="#" method="post">
+                <div>
+                    <h3>GET THE FLOWERBX NEWSLETTER</h3>
+                    <div>
+                      
+                    
+                    <form target="_self" method="post" action="http://kp4.test/wp-admin/admin-post.php?action=mailpoet_subscription_form" class="mailpoet_form mailpoet_form_form mailpoet_form_html" novalidate="" data-delay="" data-exit-intent-enabled="" data-font-family="" data-cookie-expiration-time="">
+      <input type="hidden" name="data[form_id]" value="2">
+      <input type="hidden" name="token" value="fc3cdaf5cd">
+      <input type="hidden" name="api_version" value="v1">
+      <input type="hidden" name="endpoint" value="subscribers">
+      <input type="hidden" name="mailpoet_method" value="subscribe">
+
+      <label class="mailpoet_hp_email_label" style="display: none !important;">Please leave this field empty<input type="email" name="data[email]"></label><div class="mailpoet_form_columns mailpoet_paragraph mailpoet_stack_on_mobile" style="color:;background:;"><div class="mailpoet_form_column" style="flex-basis:100%;color:;;background:;;"></div>
+</div>
+<div class="mailpoet_form_columns mailpoet_paragraph mailpoet_stack_on_mobile display: flex; 	align-items: center;" style="color:;background:;"><div class="mailpoet_form_column" style="flex-basis:250px;color:;;background:;;"><div class="mailpoet_paragraph "><input type="email" autocomplete="email" class="mailpoet_text" id="form_email_2" name="data[form_field_M2U1ZjY4NTgyNDBiX2VtYWls]" title="Email Address" value="" style="width:100%;box-sizing:border-box;background-color:#ffffff;border-style:solid;border-radius:4px !important;border-width:1px;border-color:#313131;padding:18px;margin: 0 auto;font-family:'Montserrat';font-size:16px;line-height:1.5;height:auto;" data-automation-id="form_email" placeholder="Email Address *" data-parsley-required="true" data-parsley-minlength="6" data-parsley-maxlength="150" data-parsley-type-message="This value should be a valid email." data-parsley-errors-container=".mailpoet_error_email_" data-parsley-required-message="This field is required."></div>
+</div>
+<div class="mailpoet_form_column" style="flex-basis:50px;color:;;background:;;"><div class="mailpoet_form_columns mailpoet_paragraph mailpoet_stack_on_mobile" style="color:;background:;"><div class="mailpoet_form_column mailpoet_vertically_align_center" style="flex-basis:100%;color:;;background:;;"><div class="mailpoet_paragraph "><input type="submit" class="mailpoet_submit" value=">" data-automation-id="subscribe-submit-button" data-font-family="Montserrat" style="width:100%;box-sizing:border-box;background-color:#000000;border-style:solid;border-radius:5px !important;border-width:0px;/* border-color:#313131; *//* padding:15px; */margin: 0 auto;font-family:'Montserrat';font-size:20px;line-height:1.5;height:auto;color:#ffffff;font-weight:bold;"><span class="mailpoet_form_loading"><span class="mailpoet_bounce1"></span><span class="mailpoet_bounce2"></span><span class="mailpoet_bounce3"></span></span></div>
+</div>
+</div>
+</div>
+</div>
+
+      <div class="mailpoet_message">
+        <p class="mailpoet_validate_success" style="display:none;">Thanks you!
+        </p>
+        <p class="mailpoet_validate_error" style="display:none;">        </p>
+      </div>
+    </form>
+
+
+
+
+
+
+                    </div>
+
+
+                    <!-- <form action="#" method="post">
                     <input type="hidden" name="action" value="myplugin_add_email">
                     <input type="email" name="email" placeholder="Enter your email" required>
                     <button type="submit" id="kosss-foot-subscription">></button>
-                </form>
+                </form> -->
+                </div>
+                <div>
                 <h3 class="insta-foot-social">CONNECT WITH US ON SOCIAL</h3>
-                <a href="https://www.instagram.com/theflowerbx/?hl=en" target="_blank"><span>   @THEFLOWERBX</span></a>
+                <a href="https://www.instagram.com/theflowerbx/?hl=en" target="_blank"><div class="footer-kosss-head-subscrip-div">   @THEFLOWERBX</div></a>
+                </div>
+  
             </div>
             <div class="footer-kosss-head-helpme">
                 <h3>DO YOU HAVE A QUESTION?</h3> 
