e7a213d6eea1c6bb67e2e2a0a3e60a484d362a88
Konstantyn Danylenko <k.danilenko1982@gmail.com>
2023-04-18 06:12:20 +0300

    initial commit

---
+++ wp-content/themes/shopall/page/delyvery.php
@@ -0,0 +1,45 @@
+<section class="delivry-cotent-text" >
+<div>
+<h2> NATIONAL UK DELIVERY</h2>
+<p>
+<b> Available from Wednesday to Sunday, from 7:30am-7:30pm</b> <br>
+
+You will receive your one-hour window on the day of delivery from DPD once the driver has scanned all the parcels onto the van, this is usually by 12 noon.<br>
+
+We are pleased to dispatch all UK national orders with DPD, an eco-friendly courier service with an aligned approach to sustainable practices.<br>
+</p>
+<p>
+<b> Timed Delivery</b> <br>
+
+Price on request (based on distance)<br>
+
+DPD guaranteed delivery by 10.30am & by 12pm options possible Wednesday - Saturday<br>
+
+DPD guaranteed delivery by 12pm on Sunday<br>
+
+If you have any specific requests for delivery timings ahead of the delivery date. Please contact our Customer Service team a couple of days ahead who can look into booking a direct courier (based on distance) or factoring in your timings where possible with our couriers.<br>
+</p>
+<p>
+<b> Standard Delivery (DPD) Wednesday - Saturday</b> <br>
+
+Order 3 days before<br>
+</p>
+<p>
+<b> Sunday Delivery</b> 
+
+You will need to place your order by Thursday 00:00 GMT to receive your flowers<br>
+
+ 
+
+DPD delivery is 95% guaranteed to arrive on your selected day. However, due to factors outside of our control, there is a small chance that the delivery may arrive a day later than expected.<br>
+</p>
+ 
+<p>
+<b> MONDAY & TUESDAY DELIVERY </b><br>
+
+Monday and Tuesday deliveries are available for London only. You will need to place your order by Thursday 00:00 GMT to receive your flowers the following Monday and Sunday by 00:00 GMT to receive your order the following Tuesday
+</p>
+</div>
+
+
+</section>
