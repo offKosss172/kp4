b7c9fdaaf21e632a647ada0e5140da01e866392f
SVS <svskorokhod@gmail.com>
2023-04-20 01:15:01 +0300

    new card

--- wp-content/themes/shopall/template-parts/recommendantion-product.php
+++ wp-content/themes/shopall/template-parts/recommendantion-product.php
@@ -10,8 +10,35 @@ $products = wc_get_products( $args );
 
 foreach ( $products as $product ) {
 
-    $product_one = wc_get_product( $product);
-    echo $product_one->get_image();
+    $prod_one = wc_get_product( $product);
+    //echo $prod_one->get_image();
+    //wc_get_template_part('content', 'product');
+
+    ?>
+ 
+    <div class="product type-product has-post-thumbnail shipping-taxable purchasable product-type-simple">
+        <a href="<?php echo esc_url( $prod_one->get_permalink() ) ?>" class="woocommerce-link woocommerce-loop-product__link">
+        <img width="273" height="300" src="<?php echo wp_get_attachment_image_url( $product->get_image_id(), 'thumbnail' ) ?>" 
+        class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" decoding="async" loading="lazy"><h4 class="woocommerce-loop-product__title"><?php echo $prod_one->get_name() ?></h4> 
+            <span class="price">
+                <span class="woocommerce-Price-amount amount">
+                    <bdi>
+                        <!-- <span class="woocommerce-Price-currencySymbol">€</span> -->
+                        <?php echo $prod_one->get_price_html() ?>
+                    </bdi>
+                </span>
+            </span>
+        </a>
+        <a href="?add-to-cart=<?php echo $prod_one->get_id() ?>" data-quantity="1" class="button wp-element-button product_type_simple add_to_cart_button ajax_add_to_cart" 
+        data-product_id="<?php echo $prod_one->get_id() ?>" data-product_sku="" aria-label="Додайте “<?php echo $prod_one->get_name() ?>” до кошика" rel="nofollow">Додати в кошик</a></li>
+    </div>
+ 
+<?php
+    // echo '<div class="product">';
+    // echo '<div class="product-image">' . $prod_one->get_image() . '</div>';
+    // echo '<h4 class="woocommerce-loop-product__title"><a href="' . esc_url( $prod_one->get_permalink() ) . '">'. $prod_one->get_name() .'</a></h4>';
+    // echo '<div class="product-price">' . $prod_one->get_price_html() . '</div>';
+    // echo '</div>';
 
 }
 ?>
